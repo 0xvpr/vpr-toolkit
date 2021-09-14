ec69df452b17175384a59b14692f1d7f10da0064
0xvpr <malikrb@live.com>
2021-09-14 02:39:50 -0500

    Added command line options for prettypaste

--- prettypaste.py
+++ prettypaste.py
@@ -4,6 +4,8 @@
 Author:   VPR
 Created:  September 13, 2021
 Modified: September 14, 2021
+
+Version: 0.5
 """
 
 import os
@@ -12,13 +14,18 @@ import argparse
 import platform
 import subprocess
 
+from typing import List
 from typing import Tuple
 from typing import NoReturn
 from argparse import Namespace
 from argparse import ArgumentParser
 
-PRINT_TO_STDOUT = 1
-SAVE_TO_FILE    = 2
+OP_PRINT_TO_STDOUT = 0x01
+OP_SAVE_TO_FILE    = 0x02
+OP_DELIMITER       = 0x04
+OP_PREFIX          = 0x08
+OP_SUFFIX          = 0x10
+OP_STRIP           = 0x20
 
 def init_parser() -> ArgumentParser:
     parser = ArgumentParser()
@@ -29,17 +36,55 @@ def init_parser() -> ArgumentParser:
         help="Provide destination of output file.",
         type=str
     )
+    parser.add_argument(
+        "-d", "--delimiter",
+        default="",
+        dest="delimiter",
+        help="Provide a pattern that will be stripped from the left of each line",
+        type=str
+    )
+    parser.add_argument(
+        "-i", "-p", "--prefix",
+        default="",
+        dest="prefix",
+        help="Provide a pattern that will be inserted to the left of each line",
+        type=str
+    )
+    parser.add_argument(
+        "-a", "--append", "--suffix",
+        default="",
+        dest="suffix",
+        help="Provide a pattern that will be appended to the right of each line",
+        type=str
+    )
+    parser.add_argument(
+        "-r", "-s", "--remove", "--strip",
+        default="",
+        dest="strip",
+        help="Provide a pattern that will be stripped at every occurrence",
+        type=str
+    )
 
     return parser
 
 def handle_parser(parser: ArgumentParser) -> Tuple[Namespace, int]:
-    operations = 0
+    operations = OP_PRINT_TO_STDOUT
     args = parser.parse_args()
 
-    if args.dest == "":
-        operations |= PRINT_TO_STDOUT
-    else:
-        operations |= SAVE_TO_FILE
+    if args.dest != "":
+        operations |= OP_SAVE_TO_FILE
+
+    if args.delimiter != "":
+        operations |= OP_DELIMITER
+
+    if args.prefix != "":
+        operations |= OP_PREFIX
+
+    if args.suffix != "":
+        operations |= OP_SUFFIX
+
+    if args.strip != "":
+        operations |= OP_STRIP
 
     return ( args, operations )
 
@@ -55,7 +100,7 @@ def get_arch() -> str:
         return "Unsupported OS"
 
 
-def copy_clipboard(arch: str, args: Namespace, operations: int) -> NoReturn:
+def get_clipboard_contents(arch: str, operations: int) -> List[str]:
     stdout = b""
     if arch == "WSL":
         p = subprocess.Popen("Get-Clipboard", 1024, "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe",
@@ -76,21 +121,53 @@ def copy_clipboard(arch: str, args: Namespace, operations: int) -> NoReturn:
         stdout, _ = p.communicate()
         p.wait()
 
-    # Cleanup output
-    stdout = stdout.decode().strip()
+    # Cleanup output and split
+    stdout_split = stdout.decode().strip().replace("\r\n", "\n").split("\n")
 
-    # Handle operations
-    if operations & PRINT_TO_STDOUT:
-        print(stdout, end="")
+    return stdout_split
+
+def execute_operations(stdout_split: List[str], args: ArgumentParser) -> NoReturn:
+    if operations & OP_DELIMITER:
+        new_stdout = [ ]
+
+        delim = args.delimiter
+        for line in stdout_split:
+            new_stdout.append( line.lstrip(delim).lstrip() )
+
+        stdout_split = new_stdout
         
-    if operations & SAVE_TO_FILE:
+    if operations & OP_PREFIX:
+        new_stdout = [ ]
+
+        prefix = args.prefix
+        for line in stdout_split:
+            new_stdout.append( prefix + line )
+
+        stdout_split = new_stdout
+        
+    if operations & OP_SUFFIX:
+        new_stdout = [ ]
+
+        suffix = args.suffix
+        for line in stdout_split:
+            new_stdout.append( line + suffix )
+
+        stdout_split = new_stdout
+
+    # Final formatting and output
+    stdout = "\n".join(stdout_split)
+    if operations & OP_PRINT_TO_STDOUT:
+        print(stdout, end="")
+
+    if operations & OP_SAVE_TO_FILE:
         with open(f"{args.dest}", "w") as f:
             f.write(stdout)
+        
 
 if __name__ == "__main__":
     parser = init_parser()
     args, operations = handle_parser(parser)
-
     arch = get_arch() 
-
-    copy_clipboard(arch, args, operations)
+    stdout_split = get_clipboard_contents(arch, operations)
+    
+    execute_operations(stdout_split, args)
